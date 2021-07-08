611a1fcc33dc7dfb1001a449218f38dc28da413b
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-07-08 09:04:30 +0700

    Get all reviews in a product page

--- APIv2.py
+++ APIv2.py
@@ -2,13 +2,39 @@ import urllib.request
 import json
 
 
-def get_json_ratings(itemid, limit, offset, shopid):
-	url = 'https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid={}&limit={}&offset={}&shopid={}&type=0'.format(
-		itemid, limit, offset, shopid)
-	response = urllib.request.urlopen(url)
-	data = json.loads(response.read)
-	return data
+def get_json(itemid, limit, offset, shopid):
+    url = 'https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid={}&limit={}&offset={}&shopid={}&type=0'.format(
+        itemid, limit, offset, shopid)
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+    return data
+
+
+def get_ratings_from_json(json, allow_null=True):
+    data = json['data']
+    ratings = data['ratings']
+    result = []
+    if ratings != None:
+        for r in ratings:
+            rating_star = r['rating_star']
+            comment = r['comment']
+            if comment != '' or (comment == '' and allow_null):
+                result.append((rating_star, comment))
+    return result
+
+
+def get_all_ratings(itemid, shopid, limit=6, offset=0):
+    result = []
+    while True:
+        json = get_json(itemid, limit, offset, shopid)
+        ratings = get_ratings_from_json(json)
+        if ratings == []:
+            break
+        else:
+            result.append(ratings)
+        offset += limit
+    return result
 
 
 if __name__ == '__main__':
-	print(get_json_ratings(3631471845, 6, 0, 31982992))
+    print(get_all_ratings(9154894255, 36333676))
